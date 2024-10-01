71c00463290185edabd85ca1b4953262b20f9b67
Jade Locke <jadelocke85@gmail.com>
2024-10-01 00:27:43 -0400

    removed bootsrap

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-
 import logo from '../assets/logo 2-cropped.svg'
 import menu from '../menu-icon.svg'
 import '../App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const NavigationBar = () => {
   return (
